8873d8c1b5ab2918b849df04081bcccd70f45da4
Ana <62893754+AninhasGMIT@users.noreply.github.com>
2020-04-28 08:36:51 +0100

    Exercise1
    This program identifies important values from a statement, assigning them for variables of the appropriate type and printing that message to the screen over 3 lines.

---
+++ Exercise1.java
@@ -0,0 +1,20 @@
+//This program identifies important values from a statement, assigning them to variables of the appropriate type.
+//Software Development Project - Ana Vieira, 2020.04.28
+
+
+public class Exercise1 {
+
+	public static void main (String [] args){
+
+		String name = "Mary";
+		int age = 20;
+		double weight = 65.48;
+		String favColor = "red";
+
+		//Statement using the variables and \n to print it over 3 lines.
+		System.out.println(name + " is " + age + " years old.\nShe weighs " + weight + " kilograms.\nHer favourite colour is " + favColor + ".");
+
+	}
+
+
+}
