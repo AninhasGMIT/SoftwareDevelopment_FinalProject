10a1ab01a9b645e504a73cdf3778c5ae1d322187
Ana <62893754+AninhasGMIT@users.noreply.github.com>
2020-04-28 10:51:27 +0100

    Update Exercise2.java
    Links to index

--- Exercise2.java
+++ Exercise2.java
@@ -1,5 +1,5 @@
 //This program prompts the user to enter two numbers and performs a series of arithmetic, printing the result to the screen.
-//Software Development Project - Ana Vieira, 2020.04.28
+//Software Development Project - Ana Vieira, 2020.04.25
 
 
 import java.util.Scanner;
@@ -9,7 +9,10 @@ public class Exercise2 {
 	public static void main (String [] args){
 	
 		Scanner input = new Scanner(System.in);
-		int num1, num2 = 0;
+        int num1, num2 = 0;
+        
+        System.out.println("Running Exercise 2.");
+        System.out.println();
 		
 		//Prompt for Input
 		System.out.print("Enter the first number: ");
@@ -23,7 +26,9 @@ public class Exercise2 {
 		System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
 		System.out.println(num1 + " - " + num2 + " = " + (num1-num2));
 		System.out.println(num1 + " * " + num2 + " = " + (num1*num2));
-	
+    
+        System.out.println();
+        Index.main(null); //Presents the menu again.
 	}
 
 
