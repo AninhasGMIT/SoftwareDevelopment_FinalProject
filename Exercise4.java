e0a2cdac25c0f403b492538d28b7f73806ac7eda
Ana <62893754+AninhasGMIT@users.noreply.github.com>
2020-04-28 10:54:06 +0100

    Update Exercise4.java
    Links to index

--- Exercise4.java
+++ Exercise4.java
@@ -1,4 +1,4 @@
-//This program prompts the user to enter a number, uses a control loop to make sure the number is less than 10 and greater than 0, and prints my name (Ana) to the screen as many times as the number entered.
+//This program prompts the user to enter a number, using a control loop to make sure the number is less than 10 and greater than 0 and printing my name (Ana) to the screen
 //Software Development Project - Ana Vieira, 2020.04.28
 
 import java.util.Scanner;
@@ -9,6 +9,9 @@ public class Exercise4 {
 	
         Scanner input = new Scanner(System.in);
         int num = 0;
+
+        System.out.println("Running Exercise 4.");
+        System.out.println();
 
         //Input + Control Loop
         System.out.print("Enter a number from 1 to 10: ");
@@ -27,6 +30,9 @@ public class Exercise4 {
         for (int i = 1; i < (num+1); i++){
             System.out.println(i + ". Ana");
         }
+
+        System.out.println();
+        Index.main(null); //Presents the menu again.
 	}
 
 
