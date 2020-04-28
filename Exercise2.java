94836b21d6a6e2b6037af1cad31c3c6e04b01d0b
Ana <62893754+AninhasGMIT@users.noreply.github.com>
2020-04-28 08:39:17 +0100

    Exercise2
    This program prompts the user to enter two numbers and performs a series of arithmetic, printing the result to the screen.

---
+++ Exercise2.java
@@ -0,0 +1,30 @@
+//This program prompts the user to enter two numbers and performs a series of arithmetic, printing the result to the screen.
+//Software Development Project - Ana Vieira, 2020.04.28
+
+
+import java.util.Scanner;
+
+public class Exercise2 {
+	
+	public static void main (String [] args){
+	
+		Scanner input = new Scanner(System.in);
+		int num1, num2 = 0;
+		
+		//Prompt for Input
+		System.out.print("Enter the first number: ");
+		num1 = input.nextInt();
+		System.out.print("Enter the second number: ");
+		num2 = input.nextInt();
+		
+		System.out.println();
+		
+		//Output
+		System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
+		System.out.println(num1 + " - " + num2 + " = " + (num1-num2));
+		System.out.println(num1 + " * " + num2 + " = " + (num1*num2));
+	
+	}
+
+
+}
